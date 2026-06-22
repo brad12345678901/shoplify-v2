d99df8b7e99fe5ad1ef8e35c4e27091b5220dd48
Brad <ibanezbradley@gmail.com>
2026-06-22 22:16:33 +0800

    Update Dashboard to Include a Parent Observer for Sections, also added Debug Component for debug purposes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -6,37 +6,84 @@ import AboutUsSection from "../components/sections/Dashboard/AboutUsSection";
 import ContactUsSection from "../components/sections/Dashboard/ContactUsSection";
 import NavigationHeader from "../components/NavigationHeader";
 import DebugAddProductModal from "../components/modals/Debug/DebugAddProduct";
+import ObserverHitBox from "../components/debugComponents/ObserverHitBox";
+
+type DebugOptions = {
+  addProductModal: boolean;
+  showObserverHitbox: boolean;
+};
 
 export default function Dashboard() {
-  const [activeSection, setActiveSection] = useState<string>("home");
-
-  const [showDebugAddProduct, setShowDebugAddProduct] =
-    useState<boolean>(false);
-  const toast = useToast();
+  const [activeSection, setActiveSection] = useState<string>("");
+  const [sectionVisited, setSectionVisited] = useState<Array<string>>([]);
+  const rootMarginSample = "-90% 0px -10% 0px";
 
   useEffect(() => {
-    (window as any).showDebugAddProduct = setShowDebugAddProduct;
+    const observerOptions = {
+      root: null, // uses the browser window viewport
+      rootMargin: rootMarginSample,
+      threshold: 0,
+    };
+
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (!sectionVisited.includes(entry.target.id)) {
+            setSectionVisited((prev) => [...prev, entry.target.id]);
+          }
+          setActiveSection(entry.target.id);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions,
+    );
+    const sections = document.querySelectorAll("[data-scroll-section]");
+    sections.forEach((section) => observer.observe(section));
+    return () => observer.disconnect();
+  }, []);
+
+  //DEBUG STATE
+  const [showDebugAddProduct, setShowDebugAddProduct] = useState<DebugOptions>({
+    addProductModal: false,
+    showObserverHitbox: false,
+  });
+  const toast = useToast();
+
+  //Debug Show Add Modal
+  useEffect(() => {
+    (window as any).showDebugAddProduct = (arg: boolean) =>
+      setShowDebugAddProduct((prev) => {
+        return { ...prev, addProductModal: arg };
+      });
 
     return () => {
       delete (window as any).showDebugModal;
     };
   }, []);
 
-  useEffect(() => {
-    console.log(activeSection);
-  }, [activeSection]);
-
   return (
     <>
+      {showDebugAddProduct.showObserverHitbox && (
+        <ObserverHitBox
+          rootMargin={rootMarginSample}
+          activeSection={activeSection}
+        />
+      )}
       <NavigationHeader activeSection={activeSection} />
       <DebugAddProductModal
-        show={showDebugAddProduct}
-        onClose={() => setShowDebugAddProduct(false)}
+        show={showDebugAddProduct.addProductModal}
+        onClose={() =>
+          setShowDebugAddProduct((prev) => {
+            return { ...prev, addProductModal: false };
+          })
+        }
       />
-      <HomeSection onVisible={setActiveSection} />
-      <CatalogSection onVisible={setActiveSection} />
-      <AboutUsSection onVisible={setActiveSection} />
-      <ContactUsSection onVisible={setActiveSection} />
+      <HomeSection visited={sectionVisited.includes("home")} />
+      <CatalogSection visited={sectionVisited.includes("catalog")} />
+      <AboutUsSection visited={sectionVisited.includes("aboutus")} />
+      <ContactUsSection visited={sectionVisited.includes("contactus")} />
     </>
   );
 }
