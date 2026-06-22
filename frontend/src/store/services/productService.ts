3111b5f25d5d03be7c7ddddb12a914a5f4a64bfd
Brad <ibanezbradley@gmail.com>
2026-06-22 22:16:48 +0800

    Remove Console Logs

--- frontend/src/store/services/productService.ts
+++ frontend/src/store/services/productService.ts
@@ -2,7 +2,6 @@ import { API } from "../../store/utils"
 
 export const fetchProductsApi = async () => {
     const res = await API.get("/products")
-    console.log(res);
     const data = res.data;
     return data;
 }
